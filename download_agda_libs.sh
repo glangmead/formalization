1217d1f8f0c022a1ff8307bed17a65425667e618
Greg Langmead <glangmead@gmail.com>
2022-05-24 19:50:46 -0400

    trying some post-container installation

---
+++ download_agda_libs.sh
@@ -0,0 +1,10 @@
+# install the .agda directory into the home
+cp -r .agda $HOME
+# clone agda libraries
+mkdir $HOME/proj
+cd $HOME/proj
+git clone https://github.com/agda/agda.git
+git clone https://github.com/awswan/HoTT-Agda.git HoTT-Agda-awswan
+cd HoTT-Agda-awswan && git checkout agda-2.6.1-compatible && cd ..
+git clone https://github.com/agda/cubical.git
+git clone https://github.com/UniMath/agda-unimath.git
