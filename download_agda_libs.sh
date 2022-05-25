37cf43fd3e2faaa84e19fcfcba7e26bc54e70aac
Greg Langmead <glangmead@gmail.com>
2022-05-24 20:03:17 -0400

    trying again with the agda standard library

--- download_agda_libs.sh
+++ download_agda_libs.sh
@@ -3,7 +3,8 @@ cp -r .agda $HOME
 # clone agda libraries
 mkdir $HOME/proj
 cd $HOME/proj
-git clone https://github.com/agda/agda.git
+git clone https://github.com/agda/agda-stdlib.git
+cd agda-stdlib && git checkout tags/v1.7.1 && cd ..
 git clone https://github.com/awswan/HoTT-Agda.git HoTT-Agda-awswan
 cd HoTT-Agda-awswan && git checkout agda-2.6.1-compatible && cd ..
 git clone https://github.com/agda/cubical.git
